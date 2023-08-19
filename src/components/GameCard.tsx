01b0f3c72d38dd4394b1b540c8102e34e17c1846
aadityamishraa <aadityamishra216@gmail.com>
2023-08-19 19:22:03 +0530

    skeleton set for game fetching

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -11,11 +11,11 @@ interface Props {
 }
 const GameCard = ({ game }: Props) => {
   return (
-    <Card borderRadius={10} overflow="hidden">
-      <Image src={getCroppedImageUrl( game.background_image)} />
+    <Card width="300px" borderRadius={10} overflow="hidden">
+      <Image src={getCroppedImageUrl(game.background_image)} />
       <CardBody>
         <Heading fontSize="xl">{game.name}</Heading>
-        <HStack justifyContent='space-between' paddingY={2}>
+        <HStack justifyContent="space-between" paddingY={2}>
           <PlatformIconList
             platforms={game.parent_platforms.map((p) => p.platform)}
           ></PlatformIconList>
